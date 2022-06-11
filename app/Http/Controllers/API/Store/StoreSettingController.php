a8cd26c824295ddd61c842247d2704d754248e7f
Amjad Khan <amjad.khan.contact@gmail.com>
2022-06-11 16:58:56 +0300

    - update sellerDetails, storeSetting, users functionality.

---
+++ app/Http/Controllers/API/Store/StoreSettingController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\API\Store;
+
+
+
+use App\Http\Traits\ApiResponseTrait;
+use App\Models\SellerDetails;
+use Illuminate\Http\Request;
+
+class StoreSettingController
+{
+
+    use ApiResponseTrait;
+
+    public function setStoreSetting(Request $request){
+        $requestData = $request->validate([
+            'shopping_cost' => 'required|numeric'
+        ]);
+
+        SellerDetails::updateShoppingCost($requestData['shopping_cost'], auth()->id());
+
+        return $this->apiResponse(true, 'updated!');
+    }
+}
